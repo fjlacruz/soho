3d56f528156ac37b897f93a33788f992b4a32cbe
fjlacruz <idsistemas15@gmail.com>
2020-02-02 21:44:33 -0300

    configurando enpoint

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 export default {
 	
 	//endpoint:'http://127.0.0.1:8000/api'  //endpoint para desarrollo
-	endpoint:'idsistemas15.com/proyectos/api_soho/api'  //endpoint para produccion........
+	endpoint:'idsistemas15.com/proyectos/api_soho/api'  //endpoint para produccion
 
 
 	}
