28a0f03fabfce720f073d8c528593a6f415f3a06
Javier La Cruz <idsistemas15@gmail.com>
2020-02-03 00:14:21 -0300

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,8 +1,6 @@
 export default {
 	
-	//endpoint:'http://127.0.0.1:8000/api'  //endpoint para desarrollo
-	endpoint:'dsistemas15.com/proyectos/api_soho/public/api'  //endpoint para produccion
-
+	endpoint:'http://127.0.0.1:8000/api'  //endpoint para desarrollo
 
 	}
 
