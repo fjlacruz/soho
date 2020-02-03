019477f35be59ec20b7f0b944ca6cd73ae35bf7f
fjlacruz <idsistemas15@gmail.com>
2020-02-02 21:43:20 -0300

    Actualizando

---
+++ src/config/env.js
@@ -0,0 +1,8 @@
+export default {
+	
+	//endpoint:'http://127.0.0.1:8000/api'  //endpoint para desarrollo
+	endpoint:'idsistemas15.com/proyectos/api_soho/api'  //endpoint para produccion........
+
+
+	}
+
